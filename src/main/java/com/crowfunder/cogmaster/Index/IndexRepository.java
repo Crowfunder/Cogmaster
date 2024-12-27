96aa0615a8533a0611298054014176378ae29cbf
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2024-12-27 13:02:12 +0100

    Add parameter derivation resolving

--- src/main/java/com/crowfunder/cogmaster/Index/IndexRepository.java
+++ src/main/java/com/crowfunder/cogmaster/Index/IndexRepository.java
@@ -2,8 +2,10 @@ package com.crowfunder.cogmaster.Index;
 
 import com.crowfunder.cogmaster.Configs.ConfigEntry;
 import com.crowfunder.cogmaster.Configs.ConfigReference;
+import com.crowfunder.cogmaster.Configs.ParameterArray;
 import com.crowfunder.cogmaster.Configs.Path;
 import com.crowfunder.cogmaster.Parsers.ParserService;
+
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -37,8 +39,45 @@ class IndexRepository {
 
     @PostConstruct
     public void populateIndex() {
-        index.update(parserService.parse());
-        System.out.println("Finished Parsing");
+        System.out.println("Parsing the configs, populating PathIndex...");
+        index.update(parserService.populatePathIndex());
+        System.out.println("Finished parsing");
+
+        System.out.println("Resolving derivations...");
+        resolvePathIndexDerivations();
+        System.out.println("Finished resolving");
+
+//        System.out.println("Populating ParameterIndex...");
+//        index.update(parserService.populateParameterIndex(index));
+//        System.out.println("Finished populating");
+    }
+
+    // Resolve the derivation of a config in-place
+    // We want to cache the resolved derivation in the index
+    private void resolveDerivation(Path path) {
+        ConfigEntry configEntry = resolveConfig(path);
+
+        // We only resolve derivations of derived configs
+        if (!configEntry.isDerived()) {
+            return;
+        }
+
+        ParameterArray derivedParameters = new ParameterArray();
+        ConfigEntry derivedConfig = resolveConfig(configEntry.getDerivedPath());
+        while (derivedConfig != null) {
+            derivedParameters.update(derivedConfig.getParameters());
+            derivedConfig = resolveConfig(derivedConfig.getDerivedPath());
+        }
+        configEntry.updateDerivedParameters(derivedParameters);
+
+        index.addPathIndexEntry(path, configEntry);
+    }
+
+    // Resolve and cache ALL derivations from PathIndex
+    public void resolvePathIndexDerivations() {
+        for (Path path : index.getPathIndex().keySet()) {
+            resolveDerivation(path);
+        }
     }
 
 }
