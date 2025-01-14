e7acf3e74f1743d2786a2bef4085936203c87bdf
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2025-01-14 15:17:47 +0100

    Nesting the test here makes no sense

--- src/test/java/com/crowfunder/cogmaster/Index/IndexControllerTest.java
+++ src/test/java/com/crowfunder/cogmaster/Index/IndexControllerTest.java
@@ -19,53 +19,48 @@ class IndexControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Nested
-    class APITests {
+    @Test
+    void RealConfigDeepParameter() throws Exception {
+        String path = "Accessory/Armor/Aura/Snipe Aura, Cocoa";
+        ResultActions result = mockMvc.perform(get("/api/v1/config/item?path={path}", path));
+        result.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.path.path").value(path))
+                .andExpect(jsonPath("$.parameters.hashMap.Colorizations.value.hashMap.entry.value.[0].value.hashMap.source.value.hashMap.colorization.value").value("1295"));
+    }
 
-        @Test
-        void RealConfigDeepParameter() throws Exception {
-            String path = "Accessory/Armor/Aura/Snipe Aura, Cocoa";
-            ResultActions result = mockMvc.perform(get("/api/v1/config/item?path={path}", path));
-            result.andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(jsonPath("$.path.path").value(path))
-                    .andExpect(jsonPath("$.parameters.hashMap.Colorizations.value.hashMap.entry.value.[0].value.hashMap.source.value.hashMap.colorization.value").value("1295"));
+    @Test
+    void RealConfig() throws Exception {
+        String path = "Block/Barbed Hedgehog";
+        ResultActions result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
+        result.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.path.path").value(path));
 
-        }
+        // Path without forward slashes
+        path = "Action";
+        result = mockMvc.perform(get("/api/v1/config/effect?path={path}", path));
+        result.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.path.path").value(path));
+    }
 
-        @Test
-        void RealConfig() throws Exception {
-            String path = "Block/Barbed Hedgehog";
-            ResultActions result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
-            result.andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(jsonPath("$.path.path").value(path));
+    @Test
+    void FakeConfig() throws Exception {
 
-            // Path without forward slashes
-            path = "Action";
-            result = mockMvc.perform(get("/api/v1/config/effect?path={path}", path));
-            result.andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(jsonPath("$.path.path").value(path));
-        }
+        // Fake config
+        String path = "Block/Fake Entry That Does Not Exist";
+        ResultActions result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
+        result.andExpect(status().isNotFound());
 
-        @Test
-        void FakeConfig() throws Exception {
+        // Fake config without forward slashes
+        path = "Fake Entry That Does Not Exist";
+        result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
+        result.andExpect(status().isNotFound());
 
-            // Fake config
-            String path = "Block/Fake Entry That Does Not Exist";
-            ResultActions result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
-            result.andExpect(status().isNotFound());
-
-            // Fake config without forward slashes
-            path = "Fake Entry That Does Not Exist";
-            result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
-            result.andExpect(status().isNotFound());
-
-            // Bad path with backslashes
-            path = "Block\\Barbed Hedgehog";
-            result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
-            result.andExpect(status().isNotFound());
-        }
+        // Bad path with backslashes
+        path = "Block\\Barbed Hedgehog";
+        result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
+        result.andExpect(status().isNotFound());
     }
 }
