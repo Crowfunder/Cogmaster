0f7be667a521420f33da2c47b08ff08c591ed4e8
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2025-01-14 12:58:44 +0100

    Add new unittests

--- src/main/java/com/crowfunder/cogmaster/Index/IndexController.java
+++ src/main/java/com/crowfunder/cogmaster/Index/IndexController.java
@@ -1,6 +1,7 @@
 package com.crowfunder.cogmaster.Index;
 
 import com.crowfunder.cogmaster.Configs.ConfigEntry;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -16,8 +17,15 @@ public class IndexController {
     }
 
     @GetMapping("{configName}")
-    public Optional<ConfigEntry> resolveConfigByPath(@PathVariable("configName") String configName, @RequestParam String path) {
-        return Optional.ofNullable(indexRepository.resolveConfig(configName, path));
+    public ResponseEntity<ConfigEntry> resolveConfigByPath(@PathVariable("configName") String configName, @RequestParam String path) {
+        Optional<ConfigEntry> resolvedConfig = Optional.ofNullable(indexRepository.resolveConfig(configName, path));
+        if (resolvedConfig.isPresent()) {
+            System.out.println("ok");
+            return ResponseEntity.ok(resolvedConfig.get());
+        } else {
+            System.out.println("not ok");
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
--- src/test/java/com/crowfunder/cogmaster/Index/IndexControllerTest.java
+++ src/test/java/com/crowfunder/cogmaster/Index/IndexControllerTest.java
@@ -1,22 +1,15 @@
 package com.crowfunder.cogmaster.Index;
 
-import com.crowfunder.cogmaster.Configs.ConfigEntry;
-import com.crowfunder.cogmaster.Configs.Path;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.util.Assert;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,26 +21,42 @@ class IndexControllerTest {
     @Test
     void resolveConfigByPath() throws Exception {
 
+        // Real config 1
         String path = "Accessory/Armor/Aura/Snipe Aura, Cocoa";
-        String expectedResult = "{\"implementationType\":\"com.threerings.projectx.item.config.ItemConfig$Derived\",\"derivedImplementationType\":\"com.threerings.projectx.item.config.ItemConfig$AccessoryTicket\",\"path\":{\"path\":\"Accessory/Armor/Aura/Snipe Aura, Cocoa\",\"nextPath\":\"Accessory\"},\"derivedPath\":{\"path\":\"Accessory/Parts/Base, Custom Colors\",\"nextPath\":\"Accessory\"},\"sourceConfig\":\"item\",\"parameters\":{\"empty\":false,\"hashMap\":{\"icon\":{\"value\":{\"empty\":false,\"hashMap\":{\"file\":{\"value\":\"ui/icon/inventory/icon_accessory-afront.png\",\"nested\":false}}},\"nested\":true},\"stackable\":{\"value\":\"true\",\"nested\":false},\"Accessory\":{\"value\":{\"implementationType\":\"com.threerings.config.ConfigReference\",\"sourceConfig\":\"item\",\"parameters\":{\"empty\":true,\"hashMap\":{}},\"path\":{\"path\":\"Armor/Aura/Snipe Aura, Cocoa\",\"nextPath\":\"Armor\"}},\"nested\":false},\"Icon\":{\"value\":\"ui/icon/inventory/icon_accessory-aaura_cm.png\",\"nested\":false},\"Colorizations\":{\"value\":{\"empty\":false,\"hashMap\":{\"entry\":{\"value\":[{\"value\":{\"empty\":false,\"hashMap\":{\"source\":{\"value\":{\"empty\":false,\"hashMap\":{\"colorization\":{\"value\":\"1295\",\"nested\":false}}},\"nested\":true},\"clazz\":{\"value\":\"11\",\"nested\":false}}},\"nested\":true},{\"value\":{\"empty\":false,\"hashMap\":{\"colorization\":{\"value\":\"3114\",\"nested\":false}}},\"nested\":true},{\"value\":{\"empty\":false,\"hashMap\":{\"colorization\":{\"value\":\"3370\",\"nested\":false}}},\"nested\":true}],\"nested\":false}}},\"nested\":true},\"rarity\":{\"value\":\"-1\",\"nested\":false}}},\"derivedParameters\":{\"empty\":false,\"hashMap\":{\"icon\":{\"value\":{\"empty\":false,\"hashMap\":{\"file\":{\"value\":\"ui/icon/inventory/icon_accessory-afront.png\",\"nested\":false}}},\"nested\":true},\"stackable\":{\"value\":\"true\",\"nested\":false},\"rarity\":{\"value\":\"-1\",\"nested\":false}}},\"derived\":true,\"ownParameters\":{\"empty\":false,\"hashMap\":{\"Accessory\":{\"value\":{\"implementationType\":\"com.threerings.config.ConfigReference\",\"sourceConfig\":\"item\",\"parameters\":{\"empty\":true,\"hashMap\":{}},\"path\":{\"path\":\"Armor/Aura/Snipe Aura, Cocoa\",\"nextPath\":\"Armor\"}},\"nested\":false},\"Icon\":{\"value\":\"ui/icon/inventory/icon_accessory-aaura_cm.png\",\"nested\":false},\"Colorizations\":{\"value\":{\"empty\":false,\"hashMap\":{\"entry\":{\"value\":[{\"value\":{\"empty\":false,\"hashMap\":{\"source\":{\"value\":{\"empty\":false,\"hashMap\":{\"colorization\":{\"value\":\"1295\",\"nested\":false}}},\"nested\":true},\"clazz\":{\"value\":\"11\",\"nested\":false}}},\"nested\":true},{\"value\":{\"empty\":false,\"hashMap\":{\"colorization\":{\"value\":\"3114\",\"nested\":false}}},\"nested\":true},{\"value\":{\"empty\":false,\"hashMap\":{\"colorization\":{\"value\":\"3370\",\"nested\":false}}},\"nested\":true}],\"nested\":false}}},\"nested\":true}}}}";
         ResultActions result = mockMvc.perform(get("/api/v1/config/item?path={path}", path));
-
         result.andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().string(expectedResult));
+                .andExpect(jsonPath("$.path.path").value(path));
 
+        // Real config 2
+        // Deep parameters test
         path = "Block/Barbed Hedgehog";
         result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
-
         result.andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andReturn();
+                .andExpect(jsonPath("$.path.path").value(path));
+//                .andExpect(jsonPath("$.actor.name").value(""));
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        MvcResult results = result.andReturn();
-        ConfigEntry deserialized = objectMapper.readValue(results.getResponse().getContentAsString(), ConfigEntry.class);
+        // Fake config
+        path = "Block/Fake Entry That Does Not Exist";
+        result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
+        result.andExpect(status().isNotFound());
 
-        assert(deserialized.getOwnParameters().resolveParameterPath(new Path("Variant")).isNested());
+        // Fake config without forward slashes
+        path = "Fake Entry That Does Not Exist";
+        result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
+        result.andExpect(status().isNotFound());
 
+        // Path without forward slashes
+        path = "Action";
+        result = mockMvc.perform(get("/api/v1/config/effect?path={path}", path));
+        result.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.path.path").value(path));
+
+        // Bad path with backslashes
+        path = "Block\\Barbed Hedgehog";
+        result = mockMvc.perform(get("/api/v1/config/actor?path={path}", path));
+        result.andExpect(status().isNotFound());
     }
 }
