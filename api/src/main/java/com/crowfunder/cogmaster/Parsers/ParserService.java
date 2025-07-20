aa8a9abff4aeb927b0beb632e01bc5c5a746d83a
nichita <nichitacebotari0@gmail.com>
2025-07-20 14:17:24 +0200

    make brandish test work, small refactor and comments

--- api/src/main/java/com/crowfunder/cogmaster/Parsers/ParserService.java
+++ api/src/main/java/com/crowfunder/cogmaster/Parsers/ParserService.java
@@ -2,7 +2,6 @@ package com.crowfunder.cogmaster.Parsers;
 
 import com.crowfunder.cogmaster.CogmasterConfig;
 import com.crowfunder.cogmaster.Index.Index;
-import com.crowfunder.cogmaster.Properties.PropertiesRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,17 +18,16 @@ public class ParserService {
 
     Logger logger = LoggerFactory.getLogger(ParserService.class);
     private final List<Parser> parsers;
-    private final String parseablePath;
 
     public ParserService(CogmasterConfig cogmasterConfig) {
         // Initialize existing parsers, probably not the best idea to do it like that
         // but oh well
         this.parsers = new ArrayList<>();
-        this.parseablePath = cogmasterConfig.parsers().path();
+        var parseablePath = cogmasterConfig.parsers().path();
 
         try {
             PathMatchingResourcePatternResolver r = new PathMatchingResourcePatternResolver();
-            var parserResources = r.getResources("classpath*:/" + this.parseablePath + "/*.xml");
+            var parserResources = r.getResources("classpath*:/" + parseablePath + "/*.xml");
 
             for (Resource resource : parserResources) {
                 var parserName = resource.getFilename().split("\\.")[0];
@@ -37,7 +35,7 @@ public class ParserService {
             }
 
         } catch (IOException e) {
-            logger.error("Failed to load properties from specified path: /{}/*", this.parseablePath);
+            logger.error("Failed to load properties from specified path: /{}/*", parseablePath);
             throw new RuntimeException("Failed to load properties", e);
         }
     }
